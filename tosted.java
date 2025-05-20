0104882b672035b8af2f526aada5ba3d5b3d6281
mohitkapittx <mohit.yeole@kapittx.com>
2025-05-21 00:23:19 +0530

    Update tosted.java

--- tosted.java
+++ tosted.java
@@ -5,6 +5,8 @@ public class tosted {
 	public static void main(String[] args) {
 		System.out.println("tostet");
                  System.out.println("tostet");
+
+ System.out.println("tosteyyyyyyyyyyt");
 	}
 
 }
